87a305ed2841002a6a31ea1caf1bd55b63c4aa12
Dileep K <dlpkmr706@gmail.com>
2024-03-24 17:17:36 +0530

    minor changes

--- src/interface/routes/investor.ts
+++ src/interface/routes/investor.ts
@@ -1,11 +1,31 @@
 import express, { Request, Response } from "express";
 import { login, signup } from "../controller/investorController";
+import {
+  closePosition,
+  createPosition,
+  getAllClosedPositions,
+  getAllPositions,
+  profitAndLossReport,
+  updatePosition,
+} from "../controller/positionController";
+import authenticateInvestor from "../middlewares/auth";
+import { addFund, getFund } from "../controller/fundController";
+import { getAllTransactionHistory } from "../controller/transactionHistoryController";
+import { getStockDetailsAndUpdatePosition } from "../controller/webScrapperController";
 
 const router = express.Router();
 
+router.get('/get-scraped-real-time-stock-details-and-update-position',getStockDetailsAndUpdatePosition)
 router.post("/signup", signup);
 router.post("/login", login);
-
-
+router.post("/position", authenticateInvestor, createPosition);
+router.put("/position/:id", authenticateInvestor, updatePosition);
+router.get("/position", authenticateInvestor, getAllPositions);
+router.get("/position-closed", authenticateInvestor, getAllClosedPositions);
+router.put("/add-fund",authenticateInvestor,addFund)
+router.get("/fund",authenticateInvestor,getFund)
+router.get("/transactions-history",authenticateInvestor,getAllTransactionHistory)
+router.put('/close-position/:id',authenticateInvestor,closePosition)
+router.get('/profit-and-loss-report',authenticateInvestor,profitAndLossReport)
 
 export default router;
